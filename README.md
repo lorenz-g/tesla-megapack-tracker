7595114cfb3bb96856de17f55e531172ab9765bc
lorenz <lorenz.roman.gruber@gmail.com>
2021-09-01 23:22:35 +0200

    add cars vs stationary - different marker colors on the map

--- README.md
+++ README.md
@@ -10,11 +10,19 @@ for now:
 - the generated website is in the [docs](./docs) folder (it is called docs because of github pages)
 - To generate the website, install the dependencies and run `python generate_website.py`
 - To develop it is handy to use a tool like [watchexec](https://watchexec.github.io/downloads/) to listen to file changes and then rebuild the site. 
+- to edit the CSV files, it is best to use Libre Office
 
 eventually:
 - every battery installation has a toml file located in the `battery-projects`
 - in case there are multiple stages to a project, they will be in that file
 - the naming of the toml file is `date_id_project-name`, e.g. `2020-01-01_4_first-megapack-project`
+
+for the cars vs stationary:
+- raw data is in the `cars-vs-stationary.csv` file
+- using the vehicle production numbers (from the tesla quarterly press releases)
+- estimating the avg model 3/y battery with 60kWh and the model s/x with 90kwh
+  - in the future could also add a new column use a more precise estimate per quarter, but fine for now.
+
 
 # inspiration
 
--- generate_website.py
+++ generate_website.py
@@ -17,9 +17,9 @@ def generate_link(ip):
 
 
 
-def load_projects(type_="json"):
+def load_file(type_="json", filename='projects.csv'):
     "For now from the csv, later from the toml fils"
-    with open("projects.csv") as f:
+    with open(filename) as f:
         if type_ == "json":
             reader = csv.DictReader(f)
         elif type_ == "csv":
@@ -35,9 +35,9 @@ def gen_raw_data_files():
     output_dir = os.path.join('docs', "misc")
     # write the raw data files
     with open(os.path.join(output_dir, "projects.json"), 'w') as f:
-        json.dump(load_projects(), f)
+        json.dump(load_file(), f)
     
-    rows = load_projects("csv")
+    rows = load_file("csv")
     # I think the two csv files are the same, but keep it for now.
     with open(os.path.join(output_dir, "projects.csv"), 'w') as f:
         writer = csv.writer(f)
@@ -48,13 +48,90 @@ def gen_raw_data_files():
         writer.writerows(rows)
         
 
+def gen_cars_vs_stationary(projects):
+    "prepare data to use it with bootstrap bars, a charting lib might be easier..."
+
+    info_per_quarter = load_file(filename='cars-vs-stationary.csv')
+    di = {}
+
+    # those are estimates for the average battery size
+    sx_avg_battery_kwh = 90
+    y3_avg_battery_kwh = 60
+
+    # aggregate on a yearly basis
+    max_mwh = 0
+    sum_cars = 0
+    sum_ess = 0
+    for q in info_per_quarter:
+        year = q["year"]
+        if year not in di:
+            di[year] = {"sx_mwh": 0, "y3_mwh": 0, "stat_mwh":0, "year": year}
+        
+        sx = int(q["sx"]) * sx_avg_battery_kwh / 1000
+        y3 = int(q["y3"]) * y3_avg_battery_kwh / 1000
+        ess = int(q["stat_mwh"])
+        di[year]["sx_mwh"] += sx
+        di[year]["y3_mwh"] += y3
+        di[year]["stat_mwh"] += ess
+
+        # need to find th max for the percentages
+        m = max(di[year]["sx_mwh"] + di[year]["y3_mwh"], di[year]["stat_mwh"])
+        if m > max_mwh:
+            max_mwh = m
+        
+        sum_cars += (sx + y3)
+        sum_ess += ess
+    
+    li = sorted(di.values(), key=lambda x: x["year"])
+
+    for year in li:
+        # compared to max
+        year["perc_stat"] = year["stat_mwh"] / max_mwh * 100
+        year["perc_sx"] = year["sx_mwh"] / max_mwh * 100
+        year["perc_y3"] = year["y3_mwh"] / max_mwh * 100
+
+        # compared this year
+        total = year["stat_mwh"] + year["sx_mwh"] + year["y3_mwh"]
+        perc_stat_int = int(year["stat_mwh"] / total * 100)
+        year["total_gwh"] = total / 1000
+        year["perc_cars"] = 100 - perc_stat_int
+        year["perc_stat_year"] = perc_stat_int
+    
+    total_mwh = sum_cars + sum_ess
+    total_gwh = total_mwh / 1000
+    perc_cars = int(sum_cars / total_mwh * 100)
+    li.append({
+        "year": "All Time",
+        "total_gwh": total_gwh,
+        "perc_cars": perc_cars,
+        "perc_stat_year": 100 - perc_cars,
+    })
+
+    # electcity in 2018 https://www.statista.com/statistics/280704/world-power-consumption/
+    worldwide_gwh = 23398 * 1000
+    year_in_hours = 365 * 24
+    worldwide_avg_cons_gw = worldwide_gwh / year_in_hours
+
+    return {
+        "list": li,
+        "expl": {
+            "total_gwh": total_gwh,
+            # https://www.agora-energiewende.de/en/service/recent-electricity-data/
+            "avg_power_germany_gw": 60,
+            "hours_germany": total_gwh/60,
+            "minutes_world": 60 * total_gwh / worldwide_avg_cons_gw,
+        }
+    }
+
+
+
 
 def gen_templates(projects):
     file_loader = FileSystemLoader('templates')
     env = Environment(loader=file_loader)
     output_dir = 'docs'
 
-    # augment raw data
+    # augment raw projects data
     for p in projects:
         p["status_class"] = "badge rounded-pill bg-success" if p["status"] == "operation" else ""
         
@@ -68,6 +145,8 @@ def gen_templates(projects):
             mwh = int(p["capacity mwh"])
         except:
             mwh = 0
+        p["mwh_int"] = mwh
+
         if mwh >= 100:
             smileys.append("⚡")
         
@@ -86,18 +165,15 @@ def gen_templates(projects):
 
         p["smileys"] = "".join(smileys)
 
-
     # generate the index template
     fn = 'index.jinja.html' 
-
     extra = {
-        "now": dt.datetime.utcnow()
+        "now": dt.datetime.utcnow(),
+        "cars": gen_cars_vs_stationary(projects),
     }
 
     template = env.get_template(fn)
-    output = template.render(projects=projects, extra=extra, g_l=generate_link)
-    
-    
+    output = template.render(projects=projects, extra=extra, g_l=generate_link) 
     
     with open(os.path.join(output_dir, fn.replace(".jinja", "")), 'w') as f:
         f.write(output)
@@ -112,16 +188,8 @@ def gen_templates(projects):
             f.write(template.render(p=p, extra=extra, g_l=generate_link))
 
     
-    
-
-
-
-
-
-
-
 def main():
-    projects = load_projects()
+    projects = load_file()
     gen_templates(projects)
     gen_raw_data_files()
 
